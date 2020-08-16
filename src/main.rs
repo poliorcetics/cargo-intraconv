557d85ef2aba9c40c2994685a940d968600379a8
Alexis Bourget <alexis.bourget@gmail.com>
2020-08-16 17:52:10 +0200

    Read files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::env;
+use std::fs::File;
+use std::path::Path;
+use std::io::{BufReader, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args_os().skip(1);
+
+    args.for_each(|arg| {
+        let path = Path::new(&arg);
+
+        let file = match File::open(path) {
+            Ok(file) => BufReader::new(file),
+            Err(err) => {
+                eprintln!("Failed to open file '{}': {}", path.display(), err);
+                return;
+            }
+
+        };
+
+        for (n, line) in file.lines().enumerate() {
+            let line = match line {
+                Ok(s) => s,
+                Err(err) => {
+                    eprintln!("Failed to read line {}: {}", n, err);
+                    return;
+                }
+            };
+            println!("{}", line);
+        }
+    });
 }
