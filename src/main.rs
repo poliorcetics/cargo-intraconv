30b277c22046199b54882ba4c1e605d6f9a114a3
Alexis Bourget <alexis.bourget@gmail.com>
2020-08-19 10:42:10 +0200

    Complete the replacement functions

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,23 @@ fn main() {
     args.for_each(|arg| {
         let path = Path::new(&arg);
 
+        let path_display = path.display().to_string();
+        println!("{}", &path_display);
+        // TODO: Not always perfect because of unicode, fix this
+        println!("{}\n", "=".repeat(path_display.len()));
+
         let file = match File::open(path) {
             Ok(file) => BufReader::new(file),
             Err(err) => {
-                eprintln!("Failed to open file '{}': {}", path.display(), err);
+                eprintln!("Failed to open file '{}': {}", &path_display, err);
                 return;
             }
         };
 
-        match apply_regex(file) {
+        match apply_regex(file, "std") {
             Ok(_) => (),
             Err(err) => {
-                eprintln!("Failed to handle file '{}': {}", path.display(), err);
+                eprintln!("Failed to handle file '{}': {}", &path_display, err);
                 return;
             }
         }
@@ -37,10 +42,29 @@ lazy_static! {
     // Used to detect doc comment lines, empty or not. This is the same regex
     // as `EMPTY_DOC_COMMENT` without the ending `$`.
     static ref IS_DOC_COMMENT_LINE: Regex = Regex::new(r"^\s*//(?:!|/)").unwrap();
+
+    static ref IMPL_REGEX: Regex = Regex::new(concat!(
+        r"^(?P<spaces>\s*)",
+        r"(?:impl|(?:pub(?:\(.+\))? )?trait)",
+        r"(?:<.*>)? ",
+        r"(?:.* for )?",
+        r"(?P<type>\S+)",
+        r"(?:<.*>)?",
+    )).unwrap();
+
+
+    static ref LOCAL_PATH: Regex = Regex::new(concat!(
+        r"^(?:\s*)//(?:!|/) ",
+        r"\[`?(?P<elem>.*?)`?\]: ",
+        r"(?P<elem2>.*)$",
+    )).unwrap();
+
 }
 
-fn apply_regex<R: Read>(file: BufReader<R>) -> io::Result<()> {
+fn apply_regex<R: Read>(file: BufReader<R>, krate: &str) -> io::Result<Vec<String>> {
     let mut lines = Vec::new();
+    let mut curr_impl = None;
+    let mut end_impl = String::new();
 
     for (pos, line) in file.lines().enumerate() {
         let pos = pos + 1;
@@ -58,20 +82,231 @@ fn apply_regex<R: Read>(file: BufReader<R>) -> io::Result<()> {
             }
         }
 
+        let line = match handle_comment_link(line, pos, krate) {
+            Some(line) => line,
+            None => continue,
+        };
+
+        let line = match handle_module_link(line, pos, krate) {
+            Some(line) => line,
+            None => continue,
+        };
+
+        let capture = IMPL_REGEX.captures(&line);
+        if let Some(capture) = capture {
+            end_impl = capture.name("spaces").unwrap().as_str().into();
+            end_impl.push('}');
+            curr_impl = Some(capture.name("type").unwrap().as_str().to_string());
+        }
+
+        if line == end_impl {
+            curr_impl = None;
+            end_impl.clear();
+        }
+
+        let line = if let Some(ref curr_impl) = curr_impl {
+            handle_method_anchor(line, pos, curr_impl)
+        } else {
+            line
+        };
+
         lines.push(line);
     }
 
-    Ok(())
+    Ok(lines)
+}
+
+fn handle_comment_link(line: String, pos: usize, krate: &str) -> Option<String> {
+    lazy_static! {
+        static ref COMMENT_LINK: Regex = Regex::new(concat!(
+            r"^(?P<spaces>\s*)",
+            r"//(?P<c>!|/) ",
+            r"\[(?P<elem>.*?)\]: ",
+            r"(?P<supers>\.\./)*",
+            r"(?:(?P<crate>std|core|alloc)/)?",
+            r"(?P<intermediates>(?:.*/))?",
+            r"(?:enum|struct|primitive|trait|constant|type|fn|macro)\.",
+            r"(?P<elem2>.*)\.html",
+            r"(?:#(?:method|variant|tymethod)\.(?P<additional>\S*))?$",
+        ))
+        .unwrap();
+    }
+
+    let captures = match COMMENT_LINK.captures(&line) {
+        Some(c) => c,
+        None => return Some(line),
+    };
+
+    // Preparing the new line
+    let mut new = String::with_capacity(line.len());
+
+    // Building the base of the link, which is always the same.
+    // TODO: move that to one captures group only since this is not used
+    // anywhere else.
+    new.push_str(captures.name("spaces").unwrap().as_str());
+    new.push_str("//");
+    new.push_str(captures.name("c").unwrap().as_str());
+    new.push_str(" [");
+    new.push_str(captures.name("elem").unwrap().as_str());
+    new.push_str("]: ");
+
+    // First elements like the crate or `super::`
+    if let Some(root) = captures.name("crate") {
+        let root = root.as_str();
+        new.push_str(if root == krate { "crate" } else { root });
+        new.push_str("::");
+    } else if let Some(supers) = captures.name("supers") {
+        let supers = supers.as_str();
+        let count = supers.matches("/").count();
+        // This way we won't allocate a string only to immediately drop it
+        for _ in 0..count {
+            new.push_str("super::");
+        }
+    }
+
+    // Intermediates element like a path through modules.
+    if let Some(intermediates) = captures.name("intermediates") {
+        let intermediates: &str = intermediates.as_str();
+        if intermediates.starts_with("http") {
+            return Some(line);
+        }
+        if intermediates != "./" {
+            new.push_str(&intermediates.replace("/", "::"));
+        }
+    }
+
+    new.push_str(captures.name("elem2").unwrap().as_str());
+
+    // Additional linked elements like a method or a variant
+    if let Some(additional) = captures.name("additional") {
+        new.push_str("::");
+        new.push_str(additional.as_str());
+    }
+
+    print_del(&line, pos);
+
+    // Check if the link has become a local path
+    if let Some(local) = LOCAL_PATH.captures(&new) {
+        let elem = local.name("elem").unwrap().as_str();
+        let elem2 = local.name("elem2").unwrap().as_str();
+        if elem == elem2 {
+            print_del_reason("Local path");
+            return None;
+        }
+    }
+
+    print_add(&new);
+
+    Some(new)
+}
+
+fn handle_module_link(line: String, pos: usize, krate: &str) -> Option<String> {
+    lazy_static! {
+        static ref COMMENT_MODULE: Regex = Regex::new(concat!(
+            r"^(?P<spaces>\s*)",
+            r"//(?P<c>!|/) ",
+            r"\[(?P<elem>.*?)\]: ",
+            r"(?P<supers>\.\./)*",
+            r"(?:(?P<crate>std|core|alloc)/)?",
+            r"(?P<mods>(?:.*?/)*)",
+            r"index\.html$",
+        ))
+        .unwrap();
+    }
+
+    let captures = match COMMENT_MODULE.captures(&line) {
+        Some(c) => c,
+        None => return Some(line),
+    };
+
+    // Preparing the new line
+    let mut new = String::with_capacity(line.len());
+
+    // Building the base of the link, which is always the same.
+    // TODO: move that to one captures group only since this is not used
+    // anywhere else.
+    new.push_str(captures.name("spaces").unwrap().as_str());
+    new.push_str("//");
+    new.push_str(captures.name("c").unwrap().as_str());
+    new.push_str(" [");
+    new.push_str(captures.name("elem").unwrap().as_str());
+    new.push_str("]: ");
+
+    // First elements like the crate or `super::`
+    if let Some(root) = captures.name("root") {
+        let root = root.as_str();
+        new.push_str(if root == krate { "crate" } else { root });
+        new.push_str("::");
+    } else if let Some(supers) = captures.name("supers") {
+        let supers = supers.as_str();
+        let count = supers.matches("/").count();
+        // This way we won't allocate a string only to immediately drop it
+        for _ in 0..count {
+            new.push_str("super::");
+        }
+    }
+
+    if let Some(mods) = captures.name("mods") {
+        new.push_str(mods.as_str().replace("/", "::").trim_end_matches("::"));
+    }
+
+    // TODO: fix
+
+    print_del(&line, pos);
+
+    // Check if the link has become a local path
+    if let Some(local) = LOCAL_PATH.captures(&new) {
+        let elem = local.name("elem").unwrap().as_str();
+        let elem2 = local.name("elem2").unwrap().as_str();
+        if elem == elem2 {
+            print_del_reason("Local path");
+            return None;
+        }
+    }
+
+    print_add(&new);
+
+    Some(new)
+}
+
+fn handle_method_anchor(mut line: String, pos: usize, curr_impl: &str) -> String {
+    lazy_static! {
+        static ref METHOD_ANCHOR: Regex = Regex::new(concat!(
+            r"^(?P<spaces>\s*)",
+            r"//(?P<c>!|/) ",
+            r"\[(?P<elem>.*)\]: ",
+            r"#(?:method|variant|tymethod)\.(?P<additional>\S*)$",
+        ))
+        .unwrap();
+    };
+
+    let captures = METHOD_ANCHOR.captures(&line);
+
+    if let Some(captures) = captures {
+        let spaces = captures.name("spaces").unwrap().as_str();
+        let c = captures.name("c").unwrap().as_str();
+        let elem = captures.name("elem").unwrap().as_str();
+        let additional = captures.name("additional").unwrap().as_str();
+
+        print_del(&line, pos);
+        line = format!(
+            "{}//{} [{}]: {}::{}",
+            spaces, c, elem, curr_impl, additional
+        );
+        print_add(&line);
+    }
+
+    line
 }
 
 fn print_del(line: &str, pos: usize) {
     println!("{:5}:  \"{}\"", pos, Color::Red.paint(line))
 }
 
-fn print_add(line: &str, pos: usize) {
-    println!("{:5}:  \"{}\"", pos, Color::Green.paint(line))
+fn print_add(line: &str) {
+    println!("        \"{}\"\n", Color::Green.paint(line))
 }
 
 fn print_del_reason(reason: &str) {
-    println!("        \"{}\"", Color::Yellow.paint(reason))
+    println!("        {}\n", Color::Yellow.paint(reason))
 }
