8bed0daf826a339b38d94e276e44b5831def1bfd
Alexis Bourget <alexis.bourget@gmail.com>
2020-08-20 22:50:23 +0200

    Add argh for argument handling and move path handling to its own function

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,135 @@
 use ansi_term::Color;
+use argh::FromArgs;
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, Read};
-use std::path::Path;
+use std::path::PathBuf;
+
+/// Converter from path-based links to intra-doc links for the `rust-lang/rust`
+/// project.
+///
+/// This is not perfect and the modified files should still be reviewed after
+/// running it.
+///
+/// By default it will only print the changes and not apply them, use `-a`
+/// (`--apply`) to write them.
+///
+/// If you are modifying `core` or `alloc` instead of `std`, you can pass the
+/// `-c core` (`--crate core`) flag to mark the change in the root crate.
+#[derive(FromArgs, Debug)]
+struct Args {
+    /// root crate (one of `std`, `core` or `alloc`)
+    #[argh(
+        option,
+        long = "crate",
+        short = 'c',
+        from_str_fn(check_krate),
+        default = "\"std\""
+    )]
+    krate: &'static str,
+
+    /// apply the proposed changes
+    #[argh(switch, short = 'a')]
+    apply: bool,
+
+    /// files to search links in
+    #[argh(positional)]
+    paths: Vec<PathBuf>,
+}
+
+fn check_krate(krate: &str) -> Result<&'static str, String> {
+    match krate {
+        "std" => Ok("std"),
+        "core" => Ok("core"),
+        "alloc" => Ok("alloc"),
+        _ => Err("Valid crate options are `std`, `core` and `alloc`.".into()),
+    }
+}
 
 fn main() {
-    let args = env::args_os().skip(1);
+    let args: Args = argh::from_env();
 
-    args.for_each(|arg| {
-        let path = Path::new(&arg);
+    args.paths.iter().for_each(|path| handle_path(path, args.krate));
+}
 
-        let path_display = path.display().to_string();
-        println!("{}", &path_display);
-        // TODO: Not always perfect because of unicode, fix this
-        println!("{}\n", "=".repeat(path_display.len()));
+fn handle_path(path: &PathBuf, krate: &str) {
+    // First display the path of the file that is about to be opened and tested.
+    let path_display = path.display().to_string();
+    println!("{}", &path_display);
+    // TODO: Not always perfect because of unicode, fix this.
+    println!("{}\n", "=".repeat(path_display.len()));
 
-        let file = match File::open(path) {
-            Ok(file) => BufReader::new(file),
-            Err(err) => {
-                eprintln!("Failed to open file '{}': {}", &path_display, err);
-                return;
-            }
-        };
-
-        match apply_regex(file, "std") {
-            Ok(_) => (),
-            Err(err) => {
-                eprintln!("Failed to handle file '{}': {}", &path_display, err);
-                return;
-            }
+    // Then open the file, reporting if it fails.
+    let file = match File::open(path) {
+        Ok(file) => BufReader::new(file),
+        Err(err) => {
+            eprintln!("Failed to open file '{}': {}", &path_display, err);
+            return;
         }
-    });
+    };
+
+    // Then apply the regexes to search for links.
+    match search_links(file, krate) {
+        Ok(_) => (),
+        Err(err) => {
+            eprintln!("Failed to handle file '{}': {}", &path_display, err);
+            return;
+        }
+    }
+
+    // TODO: use the `apply` flag to modify the file if need be.
 }
 
 lazy_static! {
     // An empty doc comment.
-    static ref EMPTY_DOC_COMMENT: Regex = Regex::new(r"^\s*//(?:!|/)$").unwrap();
+    static ref EMPTY_DOC_COMMENT: Regex = Regex::new(r"^\s*//[!/]$").unwrap();
 
     // Used to detect doc comment lines, empty or not. This is the same regex
     // as `EMPTY_DOC_COMMENT` without the ending `$`.
-    static ref IS_DOC_COMMENT_LINE: Regex = Regex::new(r"^\s*//(?:!|/)").unwrap();
+    static ref IS_DOC_COMMENT_LINE: Regex = Regex::new(r"^\s*//[!/]").unwrap();
 
-    static ref IMPL_REGEX: Regex = Regex::new(concat!(
-        r"^(?P<spaces>\s*)",
-        r"(?:impl|(?:pub(?:\(.+\))? )?trait)",
-        r"(?:<.*>)? ",
-        r"(?:.* for )?",
-        r"(?P<type>\S+)",
-        r"(?:<.*>)?",
-    )).unwrap();
-
-
+    // Will search for a doc comment link and be used to check if the two
+    // elements are the same, indicating a local path.
     static ref LOCAL_PATH: Regex = Regex::new(concat!(
-        r"^(?:\s*)//(?:!|/) ",
+        r"^\s*//[!/] ",
         r"\[`?(?P<elem>.*?)`?\]: ",
         r"(?P<elem2>.*)$",
     )).unwrap();
 
 }
 
-fn apply_regex<R: Read>(file: BufReader<R>, krate: &str) -> io::Result<Vec<String>> {
-    let mut lines = Vec::new();
+fn search_links<R: Read>(file: BufReader<R>, krate: &str) -> io::Result<Vec<String>> {
+    lazy_static! {
+        static ref IMPL_START: Regex = Regex::new(concat!(
+            r"^(?P<spaces>\s*)",
+            r"(?:impl|(?:pub(?:\(.+\))? )?trait)",
+            r"(?:<.*>)? ",
+            r"(?:.* for )?",
+            r"(?P<type>\S+)",
+            r"(?:<.*>)?",
+        )).unwrap();
+    }
+
+
+    let mut lines = Vec::<String>::new();
     let mut curr_impl = None;
     let mut end_impl = String::new();
 
     for (pos, line) in file.lines().enumerate() {
         let pos = pos + 1;
         let line = line?.trim_end().to_string();
-        let prev_line = lines.last().map(|e: &String| e.as_str()).unwrap_or("");
 
-        if EMPTY_DOC_COMMENT.is_match(&prev_line) {
-            if EMPTY_DOC_COMMENT.is_match(&line) {
-                print_del(&line, pos);
-                print_del_reason("Consecutives empty comment lines");
-                continue;
-            } else if !IS_DOC_COMMENT_LINE.is_match(&line) {
-                print_del(prev_line, pos - 1);
-                print_del_reason("Empty comment line at the end of a comment");
+        if let Some(prev_line) = lines.last() {
+            if EMPTY_DOC_COMMENT.is_match(prev_line) {
+                if EMPTY_DOC_COMMENT.is_match(&line) {
+                    print_del(&line, pos);
+                    print_del_reason("Consecutives empty comment lines");
+                    continue;
+                } else if !IS_DOC_COMMENT_LINE.is_match(&line) {
+                    print_del(prev_line, pos - 1);
+                    print_del_reason("Empty comment line at the end of a comment");
+                }
             }
         }
 
@@ -92,9 +143,9 @@ fn apply_regex<R: Read>(file: BufReader<R>, krate: &str) -> io::Result<Vec<Strin
             None => continue,
         };
 
-        let capture = IMPL_REGEX.captures(&line);
-        if let Some(capture) = capture {
-            end_impl = capture.name("spaces").unwrap().as_str().into();
+        if let Some(capture) = IMPL_START.captures(&line) {
+            end_impl.clear();
+            end_impl.push_str(capture.name("spaces").unwrap().as_str());
             end_impl.push('}');
             curr_impl = Some(capture.name("type").unwrap().as_str().to_string());
         }
@@ -119,10 +170,8 @@ fn apply_regex<R: Read>(file: BufReader<R>, krate: &str) -> io::Result<Vec<Strin
 fn handle_comment_link(line: String, pos: usize, krate: &str) -> Option<String> {
     lazy_static! {
         static ref COMMENT_LINK: Regex = Regex::new(concat!(
-            r"^(?P<spaces>\s*)",
-            r"//(?P<c>!|/) ",
-            r"\[(?P<elem>.*?)\]: ",
-            r"(?P<supers>\.\./)*",
+            r"^(?P<link_name>\s*//[!/] \[.*?\]: )",
+            r"(?P<supers>(?:\.\./)*)",
             r"(?:(?P<crate>std|core|alloc)/)?",
             r"(?P<intermediates>(?:.*/))?",
             r"(?:enum|struct|primitive|trait|constant|type|fn|macro)\.",
@@ -141,14 +190,7 @@ fn handle_comment_link(line: String, pos: usize, krate: &str) -> Option<String> 
     let mut new = String::with_capacity(line.len());
 
     // Building the base of the link, which is always the same.
-    // TODO: move that to one captures group only since this is not used
-    // anywhere else.
-    new.push_str(captures.name("spaces").unwrap().as_str());
-    new.push_str("//");
-    new.push_str(captures.name("c").unwrap().as_str());
-    new.push_str(" [");
-    new.push_str(captures.name("elem").unwrap().as_str());
-    new.push_str("]: ");
+    new.push_str(captures.name("link_name").unwrap().as_str());
 
     // First elements like the crate or `super::`
     if let Some(root) = captures.name("crate") {
@@ -203,10 +245,8 @@ fn handle_comment_link(line: String, pos: usize, krate: &str) -> Option<String> 
 fn handle_module_link(line: String, pos: usize, krate: &str) -> Option<String> {
     lazy_static! {
         static ref COMMENT_MODULE: Regex = Regex::new(concat!(
-            r"^(?P<spaces>\s*)",
-            r"//(?P<c>!|/) ",
-            r"\[(?P<elem>.*?)\]: ",
-            r"(?P<supers>\.\./)*",
+            r"^(?P<link_name>\s*//[!/] \[.*?\]: )",
+            r"(?P<supers>(?:\.\./)*)",
             r"(?:(?P<crate>std|core|alloc)/)?",
             r"(?P<mods>(?:.*?/)*)",
             r"index\.html$",
@@ -223,17 +263,10 @@ fn handle_module_link(line: String, pos: usize, krate: &str) -> Option<String> {
     let mut new = String::with_capacity(line.len());
 
     // Building the base of the link, which is always the same.
-    // TODO: move that to one captures group only since this is not used
-    // anywhere else.
-    new.push_str(captures.name("spaces").unwrap().as_str());
-    new.push_str("//");
-    new.push_str(captures.name("c").unwrap().as_str());
-    new.push_str(" [");
-    new.push_str(captures.name("elem").unwrap().as_str());
-    new.push_str("]: ");
+    new.push_str(captures.name("link_name").unwrap().as_str());
 
     // First elements like the crate or `super::`
-    if let Some(root) = captures.name("root") {
+    if let Some(root) = captures.name("crate") {
         let root = root.as_str();
         new.push_str(if root == krate { "crate" } else { root });
         new.push_str("::");
@@ -249,8 +282,6 @@ fn handle_module_link(line: String, pos: usize, krate: &str) -> Option<String> {
     if let Some(mods) = captures.name("mods") {
         new.push_str(mods.as_str().replace("/", "::").trim_end_matches("::"));
     }
-
-    // TODO: fix
 
     print_del(&line, pos);
 
@@ -272,27 +303,18 @@ fn handle_module_link(line: String, pos: usize, krate: &str) -> Option<String> {
 fn handle_method_anchor(mut line: String, pos: usize, curr_impl: &str) -> String {
     lazy_static! {
         static ref METHOD_ANCHOR: Regex = Regex::new(concat!(
-            r"^(?P<spaces>\s*)",
-            r"//(?P<c>!|/) ",
-            r"\[(?P<elem>.*)\]: ",
+            r"^(?P<link_name>\s*//[!/] \[.*?\]: )",
             r"#(?:method|variant|tymethod)\.(?P<additional>\S*)$",
         ))
         .unwrap();
     };
 
-    let captures = METHOD_ANCHOR.captures(&line);
-
-    if let Some(captures) = captures {
-        let spaces = captures.name("spaces").unwrap().as_str();
-        let c = captures.name("c").unwrap().as_str();
-        let elem = captures.name("elem").unwrap().as_str();
+    if let Some(captures) = METHOD_ANCHOR.captures(&line) {
+        let spaces = captures.name("link_name").unwrap().as_str();
         let additional = captures.name("additional").unwrap().as_str();
 
         print_del(&line, pos);
-        line = format!(
-            "{}//{} [{}]: {}::{}",
-            spaces, c, elem, curr_impl, additional
-        );
+        line = format!("{}{}::{}", spaces, curr_impl, additional);
         print_add(&line);
     }
 
