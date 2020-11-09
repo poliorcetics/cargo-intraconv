a36e69887886f69223b77d472a52f5eee6399d66
Alexis Bourget <alexis.bourget@gmail.com>
2020-11-09 23:05:50 +0100

    Add Link::is_item and its tests

--- src/link_analyser/link.rs
+++ src/link_analyser/link.rs
@@ -85,7 +85,7 @@ impl<'a> Link<'a> {
 
         lazy_static::lazy_static! {
             static ref ASSOC_ITEM: Regex = Regex::new(&format!(
-                "^#{}.[a-zA-Z_][a-zA-Z0-9_]*$",
+                r"^#{}\.[a-zA-Z_][a-zA-Z0-9_]*$",
                 crate::link_analyser::consts::ITEM_TYPES.as_str()
             )).unwrap();
         }
@@ -121,6 +121,68 @@ impl<'a> Link<'a> {
         }
 
         SECTION.is_match(section)
+    }
+
+    fn is_item(&self) -> bool {
+        use regex::Regex;
+        use std::path::Component;
+
+        if self.is_http() || self.is_favored() {
+            return false;
+        }
+
+        lazy_static::lazy_static! {
+            static ref SIMPLE_ITEM: Regex = Regex::new(
+                &format!(r"^{}\.[a-zA-Z_][a-zA-Z0-9_]*\.html$",
+                         crate::link_analyser::consts::ITEM_TYPES.as_str())
+            ).unwrap();
+
+            static ref WITH_ASSOCIATED: Regex = Regex::new(
+                &format!(r"^{it}\.[a-zA-Z_][a-zA-Z0-9_]*\.html#{it}\.[a-zA-Z_][a-zA-Z0-9_]*$",
+                         it = crate::link_analyser::consts::ITEM_TYPES.as_str())
+            ).unwrap();
+
+            static ref WITH_SECTION: Regex = Regex::new(
+                &format!(r"^{it}\.[a-zA-Z_][a-zA-Z0-9_]*\.html#[a-zA-Z0-9_\-\.]+$",
+                         it = crate::link_analyser::consts::ITEM_TYPES.as_str())
+            ).unwrap();
+
+            static ref RUST_IDENTIFIER: Regex = Regex::new(r"^[a-zA-Z_][a-zA-Z0-9_]*$").unwrap();
+        }
+
+        // Reverse the iterator to have easy access to the last element.
+        let mut comps = self.0.components().rev();
+        // If the last element is incorrect this cannot be an item.
+        match comps.next().expect("The last element").as_os_str().to_str() {
+            Some(last_comp) => {
+                if !(SIMPLE_ITEM.is_match(last_comp)
+                    || WITH_ASSOCIATED.is_match(last_comp)
+                    || WITH_SECTION.is_match(last_comp))
+                {
+                    return false;
+                }
+            }
+            _ => return false,
+        }
+
+        // Check all the other component to ensure they are either `.` or `..`
+        // or a valid rust identifier (a module name). If not, return `false`.
+        for comp in comps {
+            match comp {
+                Component::CurDir | Component::ParentDir => (),
+                Component::Normal(path) => match path.to_str() {
+                    Some(path) => {
+                        if !RUST_IDENTIFIER.is_match(path) {
+                            return false;
+                        }
+                    }
+                    None => return false,
+                },
+                _ => return false,
+            }
+        }
+
+        true
     }
 }
 
@@ -281,5 +343,52 @@ mod tests {
         assert!(Link(Path::new("#Section.a")).is_section());
         assert!(Link(Path::new("#rection.a")).is_section());
         assert!(Link(Path::new("#_ection.a")).is_section());
+    }
+
+    #[test]
+    fn is_item() {
+        let mut rust_item = String::with_capacity(40);
+
+        for item in crate::link_analyser::consts::ALL_ITEM_TYPES {
+            rust_item.clear();
+            rust_item.push_str(item);
+            rust_item.push_str(".Type.html");
+            assert!(Link(Path::new(&rust_item)).is_item());
+
+            rust_item.clear();
+            rust_item.push_str(item);
+            rust_item.push_str(".Type.html#method.call");
+            assert!(Link(Path::new(&rust_item)).is_item());
+
+            rust_item.clear();
+            rust_item.push_str(item);
+            rust_item.push_str(".Type.html#section-name");
+            assert!(Link(Path::new(&rust_item)).is_item());
+
+            rust_item.clear();
+            rust_item.push_str("./");
+            rust_item.push_str(item);
+            rust_item.push_str(".Type.html");
+            assert!(Link(Path::new(&rust_item)).is_item());
+
+            rust_item.clear();
+            rust_item.push_str("../");
+            rust_item.push_str(item);
+            rust_item.push_str(".Type.html");
+            assert!(Link(Path::new(&rust_item)).is_item());
+
+            rust_item.clear();
+            rust_item.push_str("../mod1/mod2/");
+            rust_item.push_str(item);
+            rust_item.push_str(".Type.html");
+            assert!(Link(Path::new(&rust_item)).is_item());
+        }
+
+        assert!(!Link(Path::new("#section")).is_item());
+        assert!(!Link(Path::new("#fn.associated_item")).is_item());
+        assert!(!Link(Path::new("https://docs.rs/regex")).is_item());
+        assert!(!Link(Path::new("http://example.com")).is_item());
+        assert!(!Link(Path::new("mod1")).is_item());
+        assert!(!Link(Path::new("../mod1/mod2/index.html#section")).is_item());
     }
 }
