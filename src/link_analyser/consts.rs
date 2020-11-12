dea76245130b87d663f52d2c83d8b7c0cbf47e94
Alexis Bourget <alexis.bourget@gmail.com>
2020-11-12 01:26:03 +0100

    Add two constants for rust identifiers and html sections

--- src/link_analyser/consts.rs
+++ src/link_analyser/consts.rs
@@ -55,82 +55,128 @@ lazy_static! {
     pub static ref ITEM_TYPES: String = format!(r"(?:{})", ALL_ITEM_TYPES.join("|"));
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub static RUST_IDENTIFIER: &'static str = r"(?:[a-zA-Z_][a-zA-Z0-9_]*)";
 
-    #[test]
-    fn link_to_treat_long_matching() {
-        // Testing spacing.
-        assert!(LINK_TO_TREAT_LONG.is_match("[name]:mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[name]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[name]:  mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[name]:\tmod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("  [name]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("\t[name]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("/// [name]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("///  [name]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("///\t[name]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match(" /// [name]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("  /// [name]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("\t/// [name]: mod1\n"));
+pub static HTML_SECTION: &'static str = r"(?:#[a-zA-Z0-9_\-\.]+)";
 
-        // Testing bangs.
-        assert!(LINK_TO_TREAT_LONG.is_match("/// [name]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("//! [name]: mod1\n"));
+#[test]
+fn link_to_treat_long_matching() {
+    // Testing spacing.
+    assert!(LINK_TO_TREAT_LONG.is_match("[name]:mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[name]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[name]:  mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[name]:\tmod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("  [name]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("\t[name]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("/// [name]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("///  [name]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("///\t[name]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match(" /// [name]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("  /// [name]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("\t/// [name]: mod1\n"));
 
-        // Testing code in links.
-        assert!(LINK_TO_TREAT_LONG.is_match("[`name`]: mod1\n"));
+    // Testing bangs.
+    assert!(LINK_TO_TREAT_LONG.is_match("/// [name]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("//! [name]: mod1\n"));
 
-        // Testing non-HTTP links
-        assert!(LINK_TO_TREAT_LONG.is_match("[name]: mod1/\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[name]: mod1/mod2\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[name]: mod1/mod2/struct.Type.html\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[name]: mod1/mod2/struct.Type.html#const.NAME\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[name]: mod1/mod2/#section\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[name]: mod1/mod2#section\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[name]: mod1/mod2/index.html#section\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[name]: mod1/mod2/index.html/#section\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[name]: ../mod1/mod2\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[name]: ./../mod1\n"));
+    // Testing code in links.
+    assert!(LINK_TO_TREAT_LONG.is_match("[`name`]: mod1\n"));
 
-        // Testing HTTP links
-        assert!(LINK_TO_TREAT_LONG.is_match("[name]: https://docs.rs/regex/\n"));
-        assert!(
-            LINK_TO_TREAT_LONG.is_match("[name]: https://docs.rs/regex/1.0.33/regex/mod1/mod2\n")
-        );
-        assert!(LINK_TO_TREAT_LONG
-            .is_match("[name]: https://github.com/poliorcetics/cargo-intraconv/issues/21\n"));
+    // Testing non-HTTP links
+    assert!(LINK_TO_TREAT_LONG.is_match("[name]: mod1/\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[name]: mod1/mod2\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[name]: mod1/mod2/struct.Type.html\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[name]: mod1/mod2/struct.Type.html#const.NAME\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[name]: mod1/mod2/#section\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[name]: mod1/mod2#section\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[name]: mod1/mod2/index.html#section\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[name]: mod1/mod2/index.html/#section\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[name]: ../mod1/mod2\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[name]: ./../mod1\n"));
 
-        // Testing all sorts of characters in the link name.
-        assert!(LINK_TO_TREAT_LONG.is_match("[azertyuiop]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[AZERTYUIOP]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[@&é\"'(§è!çà)-]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[#1234567890°_]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[•ë“‘{¶«¡Çø}—]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[Ÿ´„”’[å»ÛÁØ]–]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[æÂê®†Úºîœπ]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[ÆÅÊ‚™ŸªïŒ∏]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[‡Ò∂ƒﬁÌÏÈ¬µ]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[Ω∑∆·ﬂÎÍË|Ó]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[‹≈©◊ß~]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[›⁄¢√∫ı]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[^$ù`,;:=<]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[¨*%£?./+>]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[ô€Ù@∞…÷≠≤]: mod1\n"));
-        assert!(LINK_TO_TREAT_LONG.is_match("[Ô¥‰#¿•\\±≥]: mod1\n"));
+    // Testing HTTP links
+    assert!(LINK_TO_TREAT_LONG.is_match("[name]: https://docs.rs/regex/\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[name]: https://docs.rs/regex/1.0.33/regex/mod1/mod2\n"));
+    assert!(LINK_TO_TREAT_LONG
+        .is_match("[name]: https://github.com/poliorcetics/cargo-intraconv/issues/21\n"));
+
+    // Testing all sorts of characters in the link name.
+    assert!(LINK_TO_TREAT_LONG.is_match("[azertyuiop]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[AZERTYUIOP]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[@&é\"'(§è!çà)-]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[#1234567890°_]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[•ë“‘{¶«¡Çø}—]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[Ÿ´„”’[å»ÛÁØ]–]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[æÂê®†Úºîœπ]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[ÆÅÊ‚™ŸªïŒ∏]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[‡Ò∂ƒﬁÌÏÈ¬µ]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[Ω∑∆·ﬂÎÍË|Ó]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[‹≈©◊ß~]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[›⁄¢√∫ı]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[^$ù`,;:=<]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[¨*%£?./+>]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[ô€Ù@∞…÷≠≤]: mod1\n"));
+    assert!(LINK_TO_TREAT_LONG.is_match("[Ô¥‰#¿•\\±≥]: mod1\n"));
+}
+
+#[test]
+fn item_types() {
+    let reg = Regex::new(&ITEM_TYPES).unwrap();
+    for item in ALL_ITEM_TYPES {
+        assert!(reg.is_match(item));
     }
 
-    #[test]
-    fn item_types() {
-        let reg = Regex::new(&ITEM_TYPES).unwrap();
-        for item in ALL_ITEM_TYPES {
-            assert!(reg.is_match(item));
-        }
+    assert!(!reg.is_match("text"));
+    assert!(!reg.is_match("0BDS"));
+    assert!(!reg.is_match("sdfd"));
+    assert!(!reg.is_match("STRUCT"));
+}
 
-        assert!(!reg.is_match("text"));
-        assert!(!reg.is_match("0BDS"));
-        assert!(!reg.is_match("sdfd"));
-        assert!(!reg.is_match("STRUCT"));
-    }
+#[test]
+fn rust_identifier() {
+    let reg = Regex::new(&format!("^{}$", RUST_IDENTIFIER)).unwrap();
+
+    assert!(reg.is_match("a"));
+    assert!(reg.is_match("A"));
+    assert!(reg.is_match("_"));
+
+    assert!(reg.is_match("aa"));
+    assert!(reg.is_match("aA"));
+    assert!(reg.is_match("a0"));
+    assert!(reg.is_match("a_"));
+
+    assert!(reg.is_match("Aa"));
+    assert!(reg.is_match("AA"));
+    assert!(reg.is_match("A0"));
+    assert!(reg.is_match("A_"));
+
+    assert!(reg.is_match("_a"));
+    assert!(reg.is_match("_A"));
+    assert!(reg.is_match("_0"));
+    assert!(reg.is_match("__"));
+
+    assert!(!reg.is_match("0"));
+    assert!(!reg.is_match("."));
+    assert!(!reg.is_match("#"));
+    assert!(!reg.is_match("abc()"));
+}
+
+#[test]
+fn html_section() {
+    let reg = Regex::new(&format!("^{}$", HTML_SECTION)).unwrap();
+
+    assert!(reg.is_match("#a"));
+    assert!(reg.is_match("#A"));
+    assert!(reg.is_match("#0"));
+    assert!(reg.is_match("#_"));
+    assert!(reg.is_match("#-"));
+    assert!(reg.is_match("#."));
+    assert!(reg.is_match("#test"));
+    // While this is really an associated item, this regex will catch it all
+    // the same. It is up to functions with more information to make the
+    // difference.
+    assert!(reg.is_match("#fn.item"));
+
+    assert!(!reg.is_match("#"));
+    assert!(!reg.is_match("abc"));
 }
