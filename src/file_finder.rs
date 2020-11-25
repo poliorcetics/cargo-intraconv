0e5816f0ec6bb9342292b6e7bfcadf181810d5af
Alexis Bourget <alexis.bourget@gmail.com>
2020-11-25 18:11:00 +0100

    Greatly improve the function finding the crate names and directories

--- src/file_finder.rs
+++ src/file_finder.rs
@@ -1,31 +1,29 @@
 // Code originally from:
 // https://github.com/deadlinks/cargo-deadlinks/blob/5af27cd5b4a2ce9c21b38053461ae007e645192f/src/main.rs#L130-L174
-use std::path::PathBuf;
-
 use cargo_metadata::MetadataCommand;
 
-pub fn determine_dir() -> Vec<(PathBuf, String)> {
-    let manifest = MetadataCommand::new().no_deps().exec().unwrap_or_else(|_| {
-        eprintln!("This is not a cargo directory, pass the files explicitly");
-        ::std::process::exit(1);
-    });
+use std::path::PathBuf;
 
-    let root = manifest.workspace_root;
+pub fn crate_and_src() -> impl Iterator<Item = (String, PathBuf)> {
+    let manifest = crate::code_error!(
+        1,
+        MetadataCommand::new().no_deps().exec(),
+        "This is not a cargo directory, pass the files explicitly"
+    );
 
     // FIXME: This doesn't handle renamed packages for renames other than `-` -> `_`.
-    manifest
-        .packages
-        .into_iter()
-        .flat_map(|package| {
-            vec![
-                (
-                    root.join(&package.name).join("src"),
-                    package.name.replace("-", "_"),
-                ),
-                (root.join("src"), package.name.replace("-", "_")),
-            ]
-            .into_iter()
-        })
-        .filter(|(path, _)| path.is_dir())
-        .collect()
+    manifest.packages.into_iter().filter_map(|package| {
+        let path = package
+            .manifest_path
+            .parent()
+            .expect("A Cargo.toml cannot be the root")
+            .join("src");
+        let name = package.name.replace("-", "_");
+
+        if path.is_dir() {
+            Some((name, path))
+        } else {
+            None
+        }
+    })
 }
